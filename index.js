3236621aa9276deeb87e90ab7a0b2537886ca272
Tushar <tushar.code05@gmail.com>
2021-12-22 22:28:37 +1100

    bugs fixes

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie:{
-    httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
