f06fc8e5c24db9d917291f503e446cd0aedac8a6
tushar <tushar.code05@gmail.com>
2023-07-16 15:26:24 +0530

    updated

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ const port = process.env.PORT || 8000;
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin",'https://te-am-o.herokuapp.com');
-  // res.header("Access-Control-Allow-Origin",'http://localhost:3000'); // for developement mode
+  // res.header("Access-Control-Allow-Origin",'https://te-am-o.herokuapp.com');
+  res.header("Access-Control-Allow-Origin",'http://localhost:3000'); // for developement mode
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.header("Access-Control-Allow-Credentials",true)
